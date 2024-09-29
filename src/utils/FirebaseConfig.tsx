ec9bb63c1a3bccf69d9af425bb6b18d56430a6f1
abhishekchoudharywcs <abhishek.dev@webconsultancyservices.in>
2024-09-29 11:23:08 +0530

    fixed firebase config

--- src/utils/FirebaseConfig.tsx
+++ src/utils/FirebaseConfig.tsx
@@ -10,7 +10,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY!.toString(),
+  apiKey: process.env.FIREBASE_API_KEY?.toString(),
   authDomain: "e-commerce-next-b710f.firebaseapp.com",
   projectId: "e-commerce-next-b710f",
   storageBucket: "e-commerce-next-b710f.appspot.com",
